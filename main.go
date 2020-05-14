61e41a8ba5f7a749ad631219cd8e5dc402d6de7f
Peter Olofsson <peter.olofsson@plockmatic.se>
2020-05-14 16:00:10 +0200

    Added input flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/google/go-cmp/cmp"
@@ -8,6 +9,10 @@ import (
 )
 
 func main() {
+	var ip = flag.Int("ext", 1234, "help message for flagname")
+	flag.Parse()
+
+	fmt.Printf("IP = %v\n", *ip)
 	fmt.Println(morestrings.ReverseRunes("Peter Pan"))
 	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
